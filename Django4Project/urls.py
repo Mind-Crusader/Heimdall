0ddc290b80bc477f3572542a9aa83b34c1198019
Ranajit Nath <ranajitnath7@gmail.com>
2022-04-12 21:07:17 +0530

    Home Redirect fixed
    Signed-off-by: Ranajit Nath <ranajitnath7@gmail.com>

--- Django4Project/urls.py
+++ Django4Project/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('transaction/', include('transaction.urls', namespace='transaction')),
     path('settings/', include('settings.urls', namespace='settings')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('/', HomeLoginView.as_view(), name="home_login"),
     path('', HomeLoginView.as_view(), name="home_login"),
 ]
 
